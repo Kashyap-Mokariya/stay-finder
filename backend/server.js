4e61b5b3bf860c95bc1a43ab447041e68131aa61
Kashyap Mokariya <mokariyakashyap@gmail.com>
2025-06-23 19:19:04 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'https://stay-finder-ilqree3p5-vblxzes-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
